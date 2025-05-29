222140770927899b757a86ee7b53334ea0826e4d
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-29 16:40:12 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from 'react'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
